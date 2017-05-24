b8b9007da5b128dcd242a6e9558a641ae8b3eda4
BeckHelloWord! <444870506@qq.com>
2017-05-24 16:59:53 +0800

    注释

--- Case/js/main.js
+++ Case/js/main.js
@@ -1,7 +1,7 @@
 require.config({
     baseUrl: './js',
     paths: {
-        'jquery': ['http://cdn.bootcss.com/jquery/3.2.1/jquery.min', 'lib/jquery'],
+        'jquery': ['http://cdn.bootcss.com/jquery/3.2.1/jquery.min', 'lib/jquery'], //注意，不能添加.js文件后缀，会报错
         'workA': 'lib/workA',
         'workB': 'lib/workB'
 
